73caaa7ae0c25990e6389c3ae655a64770b6afa1
satyam <skill.improve24@gmail.com>
2024-03-31 13:19:59 +0530

    created basic model

---
+++ split.py
@@ -0,0 +1,5 @@
+import splitfolders
+# dr = './SignImage48x48'
+# splitfolders.ratio("./SignImage48x48", "./splitdataset48x48", ratio=(0.8, 0.2))
+splitfolders.ratio("SignImage48x48", "Splitdata_set48x48", ratio=(0.8, 0.2))
+
